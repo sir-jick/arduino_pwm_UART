1e739227e25e008f9ad4567557cc8d010c91cb77
Sir_Jick <bolbolezelzele@gmail.com>
2021-04-13 01:41:17 +0430

    fix the master ARD board code

--- code/master/master.ino
+++ code/master/master.ino
@@ -1,3 +1,12 @@
+// turning on a 12v DC motor via SERIAL COMMUNICATION using : arduino uno , L298 , pwm wave
+// owner : Abolfazl Safaiy pour
+// student number : 95211030
+// master : eng, Aeen mehr
+// date of construction : 1400 / 01 / 23  == Monday April 12, 2021
+// This code is to control the speed of a DC motor by a potentiometer using l298n driver
+// over serial communication
+
+
 int pot_value = 0;
 #define pot_pin A0
 // TODO find and replace correct code for master
@@ -5,12 +14,13 @@ int pot_value = 0;
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(9600); // Begin the Serial at 9600 Baud
 
 }
 
 void loop() {
   pot_value = analogRead(pot_pin);
-  Serial.println(pot_value);
-  delay(1);
+  Serial.println(pot_value);// print the value of pot reading to serial print terminal
+  Serial.write(pot_value); // Write the serial data
+  delay(10); // every 10 miliseconds repeat the program
 }
